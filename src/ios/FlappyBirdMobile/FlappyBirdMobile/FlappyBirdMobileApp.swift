75624bc8f4f584f3730165f904be187fa4788b41
Suleyman C. Ataman <suleymanc@protonmail.com>
2026-04-26 12:45:47 +0100

    feat: ios app

---
+++ src/ios/FlappyBirdMobile/FlappyBirdMobile/FlappyBirdMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdMobileApp.swift
+//  FlappyBirdMobile
+//
+//  Created by Suleyman Ataman on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ src/ios/FlappyBirdMobile/FlappyBirdMobileUITests/FlappyBirdMobileUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FlappyBirdMobileUITestsLaunchTests.swift
+//  FlappyBirdMobileUITests
+//
+//  Created by Suleyman Ataman on 26/04/2026.
+//
+
+import XCTest
+
+final class FlappyBirdMobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
